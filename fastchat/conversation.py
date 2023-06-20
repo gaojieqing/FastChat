c8cd2edaadb90a8e85bf477a18f83e0540e75d59
Jieqing Gao <jieqing.gao@ximalaya.com>
2023-06-20 19:06:55 +0800

    修改template

--- fastchat/conversation.py
+++ fastchat/conversation.py
@@ -204,7 +204,7 @@ register_conv_template(
         messages=(),
         offset=0,
         sep_style=SeparatorStyle.ADD_COLON_SINGLE,
-        sep="\n",
+        sep2="\n",
         stop_str="<human>"
     )
 )
